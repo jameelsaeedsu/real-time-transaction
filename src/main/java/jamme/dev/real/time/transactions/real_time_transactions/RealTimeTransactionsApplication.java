c1b862366b2b573e8ffa29fbffebc4b1e25778a9
jameelsaeedsu <jameel.saeed@hotmail.com>
2024-10-30 20:22:59 +0100

    - first commit

---
+++ src/main/java/jamme/dev/real/time/transactions/real_time_transactions/RealTimeTransactionsApplication.java
@@ -0,0 +1,13 @@
+package jamme.dev.real.time.transactions.real_time_transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeTransactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jamme/dev/real/time/transactions/real_time_transactions/RealTimeTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package jamme.dev.real.time.transactions.real_time_transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
